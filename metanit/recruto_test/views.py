e61f6ad1015703e0c73b410b24cd9dc042a2257a
Aubameyang17 <95569587+Aubameyang17@users.noreply.github.com>
2025-04-20 14:02:11 +0300

    .

--- metanit/recruto_test/views.py
+++ metanit/recruto_test/views.py
@@ -5,6 +5,6 @@ def index(request):
     message = request.GET.get("message")
     name = request.GET.get("name")
     data = {'message': message, 'name': name}
-    return render(request, "index1.html", context=data)
+    return render(request, "index.html", context=data)
 
 
