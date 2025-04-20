ea5dce5e70c11e872a505108c311e8b1b29a8ad6
Aubameyang17 <95569587+Aubameyang17@users.noreply.github.com>
2025-04-20 14:18:45 +0300

    new

--- metanit/metanit/settings.py
+++ metanit/metanit/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4se!x!_2i34of2fqc=v%-co)p1(dckq)_b@=pg()*4t-z24llq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
